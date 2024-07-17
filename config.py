f88d3599977cbe7c8e1c20605f35fa81c96d5c13
Dyaviacygi <expertmatio@gmail.com>
2024-07-17 19:45:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("6645342828:AAGS1uh1lQsAkVdATUpngB60X5UAYDscj0Q")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Fan_boy_78")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6465928598")
 
 
 
